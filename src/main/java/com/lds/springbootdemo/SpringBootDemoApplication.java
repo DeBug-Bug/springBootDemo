398e2ef210a72787f9ab0869c56fd85ea01b73d2
李东升 <lidongshenglife@163.com>
2019-10-23 13:39:21 +0800

    更改配置

--- src/main/java/com/lds/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/lds/springbootdemo/SpringBootDemoApplication.java
@@ -1,10 +1,8 @@
 package com.lds.springbootdemo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 /*直接在Mapper类上面添加注解@Mapper，这种方式要求每一个mapper类都需要添加此注解，麻烦
@@ -13,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 //注释掉
 //@MapperScan("com.lds.springbootdemo.mapper")
 // 用来指定配置文件的位置
-@PropertySource(value={"classpath:dbconfig.properties"})
+@PropertySource(value={"classpath:config.properties"})
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
